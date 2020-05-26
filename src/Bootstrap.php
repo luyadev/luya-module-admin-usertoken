c313c85124fe94406e48d010e5f447be8ddc079d
Basil Suter <git@nadar.io>
2020-05-26 11:34:59 +0200

    add php doc

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -4,18 +4,31 @@ namespace luya\admin\usertoken;
 
 use luya\admin\events\UserAccessTokenLoginEvent;
 use luya\admin\models\User;
+use luya\admin\Module;
 use luya\admin\usertoken\models\Token;
 use yii\base\BootstrapInterface;
 
+/**
+ * Bootstrap login by token.
+ * 
+ * @author Basil Suter <git@nadar.io>
+ * @since 1.0.0
+ */
 class Bootstrap implements BootstrapInterface
 {
+    /**
+     * {@inheritDoc}
+     */
     public function bootstrap($app)
     {
-        $app->on('eventUserAccessTokenLogin', [
-            $this, 'loginByToken'
-        ]); // replace by constant
+        $app->on(Module::EVENT_USER_ACCESS_TOKEN_LOGIN, [$this, 'loginByToken']);
     }
 
+    /**
+     * Login by Token
+     *
+     * @param UserAccessTokenLoginEvent $event
+     */
     public function loginByToken(UserAccessTokenLoginEvent $event)
     {
         $token = Token::find()->where(['token' => $event->token])->one();
