711c8b95c5bc900328187114880686dceb7183d0
Basil Suter <git@nadar.io>
2020-06-25 09:51:17 +0200

    add description

--- src/controllers/TokenController.php
+++ src/controllers/TokenController.php
@@ -13,4 +13,9 @@ class TokenController extends \luya\admin\ngrest\base\Controller
      * @var string The path to the model which is the provider for the rules and fields.
      */
     public $modelClass = 'luya\admin\usertoken\models\Token';
+
+    public function getDescription()
+    {
+        return 'Display all users which have requested a login token for the given App.';
+    }
 }
