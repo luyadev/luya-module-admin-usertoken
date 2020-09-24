de2d2e695cc8bb2737b94e291e85ac236ff98360
Basil Suter <git@nadar.io>
2020-09-24 13:53:13 +0000

    add validation error

--- tests/crud/LoginControllerTest.php
+++ tests/crud/LoginControllerTest.php
@@ -16,14 +16,21 @@ class LoginControllerTest extends UserTokenTestCase
      */
     public $controller;
 
+    /**
+     * @var NgRestModelFixture
+     */
     public $appFixture;
 
+    /**
+     * @var NgRestModelFixture
+     */
     public $tokenFixture;
 
     public function afterSetup()
     {
         parent::afterSetup();
         $this->createAdminLangFixture();
+        $this->createAdminNgRestLogFixture();
         $this->appFixture = new NgRestModelFixture(['modelClass' => App::class]);
         $this->tokenFixture = new NgRestModelFixture(['modelClass' => Token::class]);
         $this->controller = new LoginController('login', $this->app->getModule('admin'));
@@ -33,5 +40,21 @@ class LoginControllerTest extends UserTokenTestCase
     {
         $this->expectException(InvalidArgumentException::class);
         $this->controller->actionIndex();
+    }
+
+    public function testFindApp()
+    {
+        $model = $this->appFixture->newModel;
+        $model->name = 'test';
+        $model->is_multiple_auth_allowed = 1;
+        $model->save();
+        $this->assertNotEmpty($model->token);
+
+        $this->app->request->setBodyParams(['app' => $model->token]);
+
+        $this->controller->actionIndex();
+
+        // validation error because of missing email and password
+        $this->assertSame(422, $this->app->response->statusCode);
     }
 }
